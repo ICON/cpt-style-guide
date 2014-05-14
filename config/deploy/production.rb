a041628df0532d64e46497228a7c3f779a258cad
slb350 <brandon.steve@gmail.com>
2014-05-14 15:47:07 -0700

    added key to production stage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, 'master'
 server 'cpt-styles.digiman.us', user: 'cpt', roles: %w{web app}
 
  set :ssh_options, {
-#   keys: %w(~/.ssh/production.pem),
+   keys: %w(~/.ssh/staging.pem),
    forward_agent: true,
    auth_methods: %w(publickey password)
  }
