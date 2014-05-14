4106af1944ff7c82f10396eec769b686718dd7d5
slb350 <brandon.steve@gmail.com>
2014-05-14 14:39:08 -0700

    built capistrano deployment

---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+set :stage, :production
+set :branch, 'master'
+
+server 'cpt-styles.digiman.us', user: 'cpt', roles: %w{web app}
+
+ set :ssh_options, {
+#   keys: %w(~/.ssh/production.pem),
+   forward_agent: true,
+   auth_methods: %w(publickey password)
+ }
